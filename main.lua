c6858909b931d008e0aa9044723892dc2e471d62
lagemeet <80648740+lagemeet@users.noreply.github.com>
2025-02-20 22:23:13 +0200

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,22 +1,59 @@
 lilka.show_fps = true
-position_of_cube = 241
-local direction = 5 
-local min_value = 30
-local max_value = 90
+debug = false
+score = 0
+stock_position = 280
+stack = {
+    {280,280,40,255,0,0},
+    {280,300,40,0,255,0},
+    {280,320,40,0,0,255},
+    {280,340,40,255,0,255}
+    -- {280,280,40,255,0,0},
+    -- {280,300,40,0,255,0},
+    -- {280,320,40,0,0,255},
+    -- {280,340,40,255,255,255}
+
+    }
+
+position_of_cube = 160
+local direction = 5
+local min_value = 160
+local max_value = 400
 
 function lilka.update(delta)
     state = controller.get_state()
-    if not state.d.pressed then
+
+    if not state.d.just_pressed then
         position_of_cube = position_of_cube + direction
         if position_of_cube >= max_value then
             direction = -5
         elseif position_of_cube <= min_value then
             direction = 5
         end
+    else
+        place_block()
     end
+
+    if state.a.pressed then
+        util.exit()
+    end
+    
 end
 
 
+function push_and_trim(tbl, newElement)
+
+    table.insert(tbl, newElement)
+    table.remove(tbl, 1)
+
+end
+
+function update_y_axes()
+    for i = 1, #stack do
+        curr_x, curr_y, curr_w, r, g, b = table.unpack(stack[i])
+        curr_y = 280 + (20*i)
+        stack[i][2] = curr_y
+    end 
+end
 
 
 function display_cube(L, W, H, display_x, display_y,color)
@@ -51,70 +88,84 @@ function display_cube(L, W, H, display_x, display_y,color)
     for _, edge in ipairs(edges) do
         local x1, y1 = display_x/2 + projected_vertices[edge[1]][1], display_y/2 + projected_vertices[edge[1]][2]
         local x2, y2 = display_x/2 + projected_vertices[edge[2]][1], display_y/2 + projected_vertices[edge[2]][2]
-        display.draw_line(x1, y1, x2, y2, color)  
+        if (debug) then
+            display.set_cursor(x1,y1)
+            display.print(_)
+        end
+        display.draw_line(x1, y1, x2, y2, color)
     end
 end
 
+    --{280,340,40,math.random(120, 255),math.random(120, 255),math.random(120, 255)}
 
-
-function display_stock_cube(size,display_x,display_y,color)
-
-    local square_cords = {
-    {0-size,0-size},
-    {size,0-size,},
-    {size,size},
-    {0-size,size},
-    {0-size,0-size}
-    }
-    local iso_square = {}
-    function to_isometric(point)
-        local x, y = point[1], point[2]
-        local iso_x = x - y
-        local iso_y = (x + y) / 2
-        return {iso_x, iso_y}
-    end
-
-    for i, point in ipairs(square_cords) do
-        iso_square[i] = to_isometric(point)
-    end
-
-    transformed_points = {}
-    for i = 1, #iso_square - 1 do
-        local p1 = iso_square[i]
-        local p2 = iso_square[i + 1]
+function place_block()
     
-        local x1, y1 = display_x/2 + p1[1], display_y/2 + p1[2]
-        local x2, y2 = display_x/2 + p2[1], display_y/2 + p2[2]
-
-        table.insert(transformed_points, {x1, y1, x2, y2})
-    end
-    for i, point in ipairs(transformed_points) do
-        if(i > 1) then
-            display.draw_line(point[1],point[2],point[1],point[2]+10,color);
-            
+    prev_x, prev_y, prev_w = table.unpack(stack[2])
+    -- curr_x, curr_y, curr_w = table.unpack(stack[#stack])
+    if (position_of_cube < prev_x) then
+        if not (score > 0) then
+            curr_w=40
         end
-        if(i < 4)then
-            if(i > 1) then
-                display.set_cursor(point[1],point[2])
-                display.draw_line(point[1],point[2]+10,point[3],point[4]+10,color)
+            overlap = position_of_cube + curr_w - prev_x
+            if (debug) then
+                print(overlap)
             end
+            if (overlap <= 0) then
+                util.exit()
+            else
+                score = score + 1
+            end
+        local new_block = {position_of_cube,stock_position,overlap,math.random(120, 255),math.random(120, 255),math.random(120, 255)}
+        push_and_trim(stack,new_block)
+        update_y_axes()
+
+            position_of_cube = 160
+
+    elseif (position_of_cube > prev_x) then
+        overlap = prev_x + prev_w - position_of_cube
+
+            if (overlap <= 0) then
+                util.exit()
+            else
+                score = score + 1
+            end
+        local new_block = {position_of_cube,stock_position,overlap,math.random(120, 255),math.random(120, 255),math.random(120, 255)}
+        if (debug) then
+            print(overlap)
         end
+        if (score >= 1) then    -- {280,300,40,0,255,0},
+            -- {280,320,40,0,0,255},
+            -- {280,340,40,255,255,255}
         
+            curr_w = overlap
+        end
+        push_and_trim(stack,new_block)
+        update_y_axes()
+
+            position_of_cube = 160
+
+    
     end
+ 
+
 end
+
+
 
 function lilka.draw()   
     display.fill_screen(display.color565(0, 0, 0))
-    display_cube(40,40,20,280,380,display.color565(0, 255, 0))
-    display_cube(40,40,20,280,400,display.color565(255, 255, 0))
-    display_cube(40,40,20,position_of_cube,330,display.color565(255, 0, 0))
-    --function display_cube(L, W, H, display_x, display_y,color)
-    display_cube(position_of_cube,40,position_of_cube,280,100,display.color565(255,255,255))
+    display.set_cursor(200,200)
+    display.print(score)
 
-    if state.a.pressed then
-        util.exit()
+    for i=#stack,2,-1 do 
+        curr_x, curr_y, curr_w, r, g, b = table.unpack(stack[i])
+        display_cube(curr_w,40,20,curr_x,curr_y,display.color565(r,g,b))
+        i = i - 1
     end
-    
+    curr_x, curr_y, curr_w, r, g, b = table.unpack(stack[4])    
+    display_cube(40,curr_w,20,position_of_cube,stock_position,display.color565(255,255,255))
+
+
 end
 
 function lilka.init()
